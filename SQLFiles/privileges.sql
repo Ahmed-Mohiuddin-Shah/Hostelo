0a3959adee94672b750819b5234bd13a20067f70
Hamza <muhammadhamzam1486@gmail.com>
2023-12-22 18:49:35 +0500

    chore: change previliges

--- SQLFiles/privileges.sql
+++ SQLFiles/privileges.sql
@@ -1,6 +1,8 @@
 create user 'ahmed'@'%' IDENTIFIED BY '1234';
 create user 'aqsa'@'%' IDENTIFIED BY '1234';
+create user 'api'@'%' IDENTIFIED BY 'ThePassword';
 
 GRANT all privileges ON *.* TO 'ahmed'@'%' with grant option;
 GRANT all privileges ON *.* TO 'aqsa'@'%' with grant option;
+GRANT select ON hostelo.user TO 'api'@'%' with grant option;
 flush privileges;
