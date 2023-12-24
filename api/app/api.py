5bc952e4c61b43da0b813cfc9728dcfd05e6107d
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-12-24 18:44:46 +0500

    Implement APIRouter

--- api/app/api.py
+++ api/app/api.py
@@ -1,34 +1,19 @@
 import time
-from fastapi import FastAPI, Body, Depends, Request
+from fastapi import  FastAPI, Body, Depends, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
-
-from app.auth.auth_bearer import JWTBearer
-from app.auth.auth_handler import signAndGetJWT, decodeJWT
+from app.routes.authentication import router
 
 #################################################
 
-from decouple import config
-from mysql.connector import connect, Error
-
-try:
-  connection = connect(
-        host = config("mySQLServerIP"),
-        user = config("apiUserName"),
-        password = config("apiPassword")
-    )
-except Error as e:
-    print(e)
-
-cursor = connection.cursor()
-cursor.execute("USE Hostelo")
-
 #################################################
 
-app = FastAPI()
 
 origins = ["*"]
 
-app.add_middleware(
+def get_application() -> FastAPI:
+    application = FastAPI(title="Hostelo API", debug=True)
+
+    application.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
@@ -36,29 +21,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/auth/signin", tags=["Authentication"])
-async def sign_in(request: Request):
-    request_json = await request.json()
-    query = f"SELECT * FROM `user` WHERE `username`='{request_json.get('username')}' AND `password`='{request_json.get('password')}'"
-    cursor.execute(query)
+    application.include_router(router, prefix="/api")
 
-    user = cursor.fetchone()
-    if user is None:
-        return {
-            "status": False,
-            "msg": "Username or password is incorrect"
-        }
-    
-    username, _, role = user
-    token = signAndGetJWT({"username": username, "role": role})
-    return {
-        "status": True,
-        "msg": "Login successful",
-        "token": token
-    }
+    return application
 
-@app.post("/auth/test-token", tags=["Authentication"])
-async def test(request: Request):
-    request_json = await request.json()
-    token = request_json.get('token')
-    return decodeJWT(token)
+
+app = get_application()
---
+++ api/app/routes/authentication.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Body, Depends, HTTPException, Request
+
+from app.auth.auth_bearer import JWTBearer
+from app.auth.auth_handler import signAndGetJWT, decodeJWT
+
+from decouple import config
+from mysql.connector import connect, Error
+
+try:
+  connection = connect(
+        host = config("mySQLServerIP"),
+        user = config("apiUserName"),
+        password = config("apiPassword")
+    )
+except Error as e:
+    print(e)
+
+cursor = connection.cursor()
+cursor.execute("USE Hostelo")
+
+router = APIRouter()
+
+@router.post("/auth/signin", tags=["Authentication"])
+async def sign_in(request: Request):
+    request_json = await request.json()
+    query = f"SELECT * FROM `user` WHERE `username`='{request_json.get('username')}' AND `password`='{request_json.get('password')}'"
+    cursor.execute(query)
+
+    user = cursor.fetchone()
+    if user is None:
+        return {
+            "status": False,
+            "msg": "Username or password is incorrect"
+        }
+    
+    username, _, role = user
+    token = signAndGetJWT({"username": username, "role": role})
+    return {
+        "status": True,
+        "msg": "Login successful",
+        "token": token
+    }
+
+@router.post("/auth/validate-token", tags=["Authentication"])
+async def validate_tokem(request: Request):
+    request_json = await request.json()
+    token = request_json.get('token')
+    return decodeJWT(token)
