d42f7228b18762edcf058225ccbb28f74b16fa78
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-12-25 18:55:28 +0500

    Add connection to every route

--- api/app/routes/students.py
+++ api/app/routes/students.py
@@ -2,6 +2,27 @@ from fastapi import APIRouter, Body, Depends, Request
 
 students_router = APIRouter()
 
+########################################################
+
+from decouple import config # type: ignore
+from mysql.connector import connect, Error
+
+try:
+    connection = connect(
+        host = config("mySQLServerIP"),
+        user = config("apiUserName"),
+        password = config("apiPassword")
+    )
+except Error as e:
+    print(e)
+
+cursor = connection.cursor()
+cursor.execute("USE Hostelo")
+
+print("Connected to MySQL Server")
+
+########################################################
+
 @students_router.post("/students", tags=["Students"])
 async def get_total_students(request: Request):
 
