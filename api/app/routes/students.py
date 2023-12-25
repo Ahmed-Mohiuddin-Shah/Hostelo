434f6ae5a3a04138c22ce243d7b4a99d2891d4a8
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-12-25 19:58:40 +0500

    move number of free slots to rooms route

--- api/app/routes/students.py
+++ api/app/routes/students.py
@@ -38,9 +38,8 @@ print("Connected to MySQL Server")
 
 ########################################################
 
-@students_router.get("/students/no-of-students", tags=["Student"])
+@students_router.get("/number-of-students", tags=["Student"])
 async def get_total_students(request: Request):
-    request_json = await request.json
     query  = f"SELECT COUNT(student_id) FROM `Student` "
     cursor.execute(query)
 
@@ -50,52 +49,10 @@ async def get_total_students(request: Request):
        return {
             "status": True,
             "msg": "Retrieval successful",
-            "data": {
-               "count": total_students
-            }
+            "data": {"count" : total_students[0]}
         }
     else:
        return {
           "status" : False,
           "msg" : "Retrieval Not Successful"
        }
-    
-@students_router.get("/students/available-student-slots", tags=["Student"])           
-async def get_available_student_slots(request: Request):
-    request_json = await request.json
-    query = f"SELECT COUNT(`single(attach bath)`) AS No_ofSingle_Rooms
-        FROM `roomtype` "
-    cursor.execute(query)
-    single_occupancy = cursor.fetchone()
-
-    query = f"SELECT COUNT(`triple`) FROM `roomtype` "
-    cursor.execute(query)
-    triple_occupancy = cursor.fetchone()
-
-    query = f"SELECT COUNT(`double(attach bath)`) FROM `roomtype`"
-    cursor.execute(query)
-    biseater_w_attachbath = cursor.fetchone()
-
-    query = f"SELECT COUNT(`double`)  FROM `roomtype`"
-    cursor.execute(query)
-    biseater = cursor.fetchone()
-
-    total_rooms =  single_occupancy + triple_occupancy + biseater_w_attachbath + biseater
-
-    cursor.execute(f"SELECT COUNT(student_id) FROM `Student` ")
-    total_students = cursor.fetchone()
-    available_student_slots = total_rooms - total_students
-    
-    if available_student_slots:
-       return {
-            "status": True,
-            "msg": "Retrieval successful",
-            "data": {
-               "count": available_student_slots
-            }
-        }
-    else:
-       return {
-          "status" : False,
-          "msg" : "Retrieval Not Successful"
-       }       
