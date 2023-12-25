a230e0ee402bf5d799bd9c67cfca45d0300467ac
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-12-25 18:55:10 +0500

    Create new route rooms

---
+++ api/app/routes/rooms.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Body, Depends, Request
+
+rooms_router = APIRouter()
+
+########################################################
+
+from decouple import config # type: ignore
+from mysql.connector import connect, Error
+
+try:
+    connection = connect(
+        host = config("mySQLServerIP"),
+        user = config("apiUserName"),
+        password = config("apiPassword")
+    )
+except Error as e:
+    print(e)
+
+cursor = connection.cursor()
+cursor.execute("USE Hostelo")
+
+print("Connected to MySQL Server")
+
+########################################################
+
+@rooms_router.get("/room-types", tags=["Students"])
+async def get_room_types(request: Request):
+
+    cursor.execute("SELECT * FROM `roomtype`")
+    room_types = cursor.fetchall()
+
+    return {
+        "data": room_types,
+        "status": True,
+        "msg": "Get room types successful"
+    }
