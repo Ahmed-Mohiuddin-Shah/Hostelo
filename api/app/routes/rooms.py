502cea183ca467a9f14e0b0f2758ec0684a47cd0
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-12-25 19:59:29 +0500

    add number-of-free-slots to rooms route

--- api/app/routes/rooms.py
+++ api/app/routes/rooms.py
@@ -23,7 +23,7 @@ print("Connected to MySQL Server")
 
 ########################################################
 
-@rooms_router.get("/room-types", tags=["Students"])
+@rooms_router.get("/room-types", tags=["Rooms"])
 async def get_room_types(request: Request):
 
     query = "SELECT * FROM `roomtype`"
@@ -35,3 +35,40 @@ async def get_room_types(request: Request):
         "status": True,
         "msg": "Get room types successful"
     }
+
+@rooms_router.get("/number-of-free-slots", tags=["Rooms"])
+async def get_total_free_slots(request: Request):
+    query = f"SELECT COUNT(*) FROM `student`"
+    cursor.execute(query)
+    total_students = cursor.fetchone()
+    query = f"SELECT COUNT(*) FROM `room` NATURAL JOIN `roomtype` WHERE `type_id` = 1"
+    cursor.execute(query)
+    single_rooms = cursor.fetchone()
+    query = f"SELECT COUNT(*) FROM `room` NATURAL JOIN `roomtype` WHERE `type_id` = 2"
+    cursor.execute(query)
+    double_community = cursor.fetchone()
+    query = f"SELECT COUNT(*) FROM `room` NATURAL JOIN `roomtype` WHERE `type_id` = 3"
+    cursor.execute(query)
+    triple_room = cursor.fetchone()
+    query = f"SELECT COUNT(*) FROM `room` NATURAL JOIN `roomtype` WHERE `type_id` = 4"
+    cursor.execute(query)
+    double_attached = cursor.fetchone()
+
+    available_student_slots = single_rooms[0] + (double_community[0] * 2) + (triple_room[0] * 3) + (double_attached[0] * 2) - total_students[0] # type: ignore
+    if available_student_slots:
+       return {
+            "status": True,
+            "msg": "Retrieval successful",
+            "data": {
+               "count": available_student_slots
+            }
+        }
+    else:
+       return {
+          "status" : False,
+          "msg" : "Retrieval Not Successful"
+       } 
+
+@rooms_router.post("/add-room", tags=["Rooms"])
+async def add_room(request: Request):
+    pass
