1497180f1935ec3b30e75d1cda832e51e52872b5
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-12-26 19:12:35 +0500

    add mess-on-date endpoint

--- api/app/routes/mess.py
+++ api/app/routes/mess.py
@@ -27,7 +27,7 @@ print("Connected to MySQL Server")
 
 @mess_router.get("/mess-off-students", tags=["Mess"])
 async def get_mess_off_students(request: Request):
-    cursor.execute(f"SELECT `student_id`,`name`,`room_number`, `end_date` FROM `student` NATURAL JOIN `messoff` WHERE `end_date` > CURRENT_DATE() ORDER BY `end_date` DESC")
+    cursor.execute(f"SELECT `student_id`,`name`,`room_number`, DATE_ADD(end_date, INTERVAL 1 DAY) FROM `student` NATURAL JOIN `messoff` WHERE `end_date` > CURRENT_DATE() ORDER BY `end_date` DESC")
     result = cursor.fetchall()
     if result != None:
         return{
@@ -41,4 +41,25 @@ async def get_mess_off_students(request: Request):
         return {
                 "status" : False,
                 "msg" : "Retrieval Not Successful"
-        } 
+        }
+
+@mess_router.get("/mess-on-date", tags=["Mess"])
+async def get_mess_on_date(request: Request):
+
+    cursor.execute(f"""SELECT DATE_ADD(end_date, INTERVAL 1 DAY) as mess_on_date from messoff JOIN student ON student.student_id=messoff.student_id""")
+    
+    result = cursor.fetchall()
+
+    if result:
+        return{
+                "status": True,
+                "msg": "Retrieval successful",
+                "data": {
+                    "MESS_ON_DATE": result
+                }
+        }
+    else:
+        return {
+                "status" : False,
+                "msg" : "Retrieval Not Successful"
+        }
