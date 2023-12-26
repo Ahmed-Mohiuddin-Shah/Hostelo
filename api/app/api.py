1835ccc8e56352cccacfb73171de1567f79b4561
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-12-26 15:46:54 +0500

    add assets router to api

--- api/app/api.py
+++ api/app/api.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes.authentication import auth_router
 from app.routes.students import students_router
 from app.routes.rooms import rooms_router
+from app.routes.assets import assets_router
 
 def get_application() -> FastAPI:
     application = FastAPI(title="Hostelo API", debug=True)
@@ -18,8 +19,8 @@ def get_application() -> FastAPI:
     application.include_router(auth_router, prefix="/api/auth")
     application.include_router(students_router, prefix="/api/students")
     application.include_router(rooms_router, prefix="/api/rooms")
+    application.include_router(assets_router, prefix="/api/assets")
 
     return application
-
 
 app = get_application()
