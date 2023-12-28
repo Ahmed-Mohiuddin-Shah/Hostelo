cc63f2f937ae9df73628ddb5c82cafdfc18dfc34
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-12-28 18:45:10 +0500

    create MySQL Connection Class

---
+++ api/app/my_sql_connection_cursor.py
@@ -0,0 +1,21 @@
+from decouple import config # type: ignore
+from mysql.connector import connect, Error
+
+
+class DataBaseCursor:
+    def __init__(self):
+        try:
+            self.connection = connect(
+                host = config("mySQLServerIP"),
+                user = config("apiUserName"),
+                password = config("apiPassword")
+            )
+        except Error as e:
+            print(e)
+
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("USE Hostelo")
+        print("Connected to MySQL Server")
+
+databaseCursor = DataBaseCursor()
+cursor = databaseCursor.cursor
