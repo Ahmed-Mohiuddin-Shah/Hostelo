c3ded42b4e278b0948c8d4fd58dc9d523cd94f3f
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-12-28 12:28:10 +0500

    change api file name

---
+++ api/app/api_config.py
@@ -0,0 +1,32 @@
+from os import close
+from fastapi import  FastAPI, Body, Depends
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routes.authentication import auth_router
+from app.routes.students import students_router
+from app.routes.rooms import rooms_router
+from app.routes.assets import assets_router
+from app.routes.complaints import complaints_router
+from app.routes.mess import mess_router
+
+def get_application() -> FastAPI:
+    application = FastAPI(title="Hostelo API", debug=True)
+
+    application.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+    application.include_router(auth_router, prefix="/api/auth")
+    application.include_router(students_router, prefix="/api/students")
+    application.include_router(rooms_router, prefix="/api/rooms")
+    application.include_router(assets_router, prefix="/api/assets")
+    application.include_router(complaints_router, prefix="/api/complaints") 
+    application.include_router(mess_router, prefix="/api/mess")
+
+    return application
+
+app = get_application()
