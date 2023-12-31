724eabbb8514a75c3dadd9ce787b890005400d6a
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-12-31 19:07:27 +0500

    removed redundant route

--- api/app/routes/mess.py
+++ api/app/routes/mess.py
@@ -22,24 +22,3 @@ async def get_mess_off_students(request: Request):
                 "status" : False,
                 "msg" : "Retrieval Not Successful"
         }
-
-@mess_router.get("/mess-on-date", tags=["Mess"])
-async def get_mess_on_date(request: Request):
-
-    cursor.execute(f"""SELECT DATE_ADD(end_date, INTERVAL 1 DAY) as mess_on_date from messoff JOIN student ON student.student_id=messoff.student_id""")
-    
-    result = cursor.fetchall()
-
-    if result:
-        return{
-                "status": True,
-                "msg": "Retrieval successful",
-                "data": {
-                    "MESS_ON_DATE": result
-                }
-        }
-    else:
-        return {
-                "status" : False,
-                "msg" : "Retrieval Not Successful"
-        }
