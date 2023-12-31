f43fc354169ad22f3f977d95e08b5cb5dbf2f465
Hamza <muhammadhamzam1486@gmail.com>
2023-12-31 19:09:47 +0500

    feat: add mess off link to the sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -19,6 +19,7 @@ import {
   FaMegaport,
   FaPlugCircleBolt,
   FaReceipt,
+  FaSnowflake,
   FaSquareCheck,
   FaUsers,
 } from "react-icons/fa6";
@@ -117,6 +118,11 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           path: "/all-attendance",
         },
       ],
+    },
+    {
+      name: "Mess off",
+      path: "/mess-off",
+      Icon: () => <FaSnowflake className="fill-current text-lg" />,
     },
     {
       name: "Invoice",
@@ -243,9 +249,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <aside
       ref={sidebar}
-      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
-        sidebarOpen ? "translate-x-0" : "-translate-x-full"
-      } ${authContext.isLoggedIn ? "" : "hidden"}`}
+      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } ${authContext.isLoggedIn ? "" : "hidden"}`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
