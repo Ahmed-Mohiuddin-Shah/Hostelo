61dfbdd2a5f5372970d5f064c62a9ab612674728
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-01 13:36:22 +0500

    implement FUnction to get status

--- SQLFiles/createStoreProcedures.sql
+++ SQLFiles/createStoreProcedures.sql
@@ -7,6 +7,7 @@ CREATE PROCEDURE GET_STATUS(IN BOOLEANSTATUS BOOLEAN
 	SELECT
 	SELECT
 	SELECT
+	SELECT
 	SELECT (
 	        CASE
 	            WHEN booleanStatus <> 0 THEN 'pending'
@@ -20,3 +21,44 @@ CREATE PROCEDURE GET_STATUS(IN BOOLEANSTATUS BOOLEAN
 DROP PROCEDURE IF EXISTS `GET_STATUS`;
 
 CALL `GET_STATUS`(0);
+
+SELECT
+    `complaint_id`,
+    `student_id`,
+    `name`,
+    `room_number`,
+    `title`,
+    `description`,
+    `GET_STATUS`(`status`)
+FROM `complaintandquery`
+    NATURAL JOIN `student`
+ORDER BY
+    `status` DESC,
+    `complaint_id` DESC
+
+SELECT
+    `complaint_id`,
+    `student_id`,
+    `name`,
+    `room_number`,
+    `title`,
+    `description`,
+    `GET_STATUS`(`status`)
+FROM `complaintandquery`
+    NATURAL JOIN `student`
+ORDER BY
+    `status` DESC,
+    `complaint_id` DESC
+
+CREATE FUNCTION GET_STATUS(BOOLEANSTATUS BOOLEAN) RETURNS 
+VARCHAR(10) DETERMINISTIC BEGIN RETURN 
+	RETURN (
+	        CASE
+	            WHEN booleanStatus <> 0 THEN 'pending'
+	            ELSE 'resolved'
+	        END
+	    );
+	END;
+
+
+DROP FUNCTION IF EXISTS `GET_STATUS`;
