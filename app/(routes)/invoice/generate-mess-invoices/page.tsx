0d1242a31e69c300b6c4e1c2efcc4bd00c3c51d9
Hamza <muhammadhamzam1486@gmail.com>
2024-01-01 19:41:30 +0500

    chore: remove placeholder

--- app/(routes)/invoice/generate-mess-invoices/page.tsx
+++ app/(routes)/invoice/generate-mess-invoices/page.tsx
@@ -120,7 +120,6 @@ export default function Page() {
             </label>
             <input
               type="month"
-              placeholder="Per day cost of mess"
               id="invoiceMonth"
               name="invoiceMonth"
               className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
