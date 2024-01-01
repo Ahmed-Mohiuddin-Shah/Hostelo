3d09d717d18c96706f16a202718affefca54fdb4
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-01 23:41:07 +0500

    Fix JWT token expiration bug

--- api/app/auth/auth_handler.py
+++ api/app/auth/auth_handler.py
@@ -14,24 +14,32 @@ def signAndGetJWT(data: Dict) -> str:
         **data,
         "expires": time.time() + 600
     }
-    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)  #type: ignore
     return token
 
 
 def decodeJWT(token: str) -> dict:
     try:
-        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])  #type: ignore
         status = time.time() - decoded_token["expires"] <= 0
-        return {"status" : status}
+
+        ## TODO: Uncomment this after testing
+
+        # if status is True:
+        #     return decoded_token
+        # else:
+
+        # return {"status" : status}
+        return decoded_token
     except jwt.DecodeError:
         return {
             "error": "Invalid token"
         }
     
 def getRole(token: str) -> str:
-    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])  #type: ignore
     return decoded_token["role"]
 
 def getUsername(token: str) -> str:
-    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])  #type: ignore
     return decoded_token["username"]
