1b18460bb6215e9a9bb31f233f875d52e674fe14
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-01 17:53:33 +0500

    add update profile picture endpoint

--- api/app/routes/users.py
+++ api/app/routes/users.py
@@ -4,3 +4,22 @@ from app.my_sql_connection_cursor import cursor, connection # type: ignore
 
 users_router = APIRouter()
 
+@users_router.post("/update-profile-picture", tags=["Users"])
+async def update_profile_picture(request: Request):
+    request_json = await request.json()
+    username = request_json.get('username')
+    image_url = request_json.get('image_url')
+    query = f"UPDATE `user` SET `image_url`='{image_url}' WHERE `username`='{username}'"
+    try:
+        cursor.execute(query)
+        connection.commit()
+        return {
+            "status": True,
+            "msg": "Profile picture updated successfully"
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "status": False,
+            "msg": "Something went wrong"
+        }
