336ca3782d9471ecce2d36d7aaaf34c6151c06ab
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-01 18:15:34 +0500

    add new role name in token

--- api/app/routes/users.py
+++ api/app/routes/users.py
@@ -1,5 +1,6 @@
 from uu import Error
 from fastapi import APIRouter, Body, Depends, Request
+from app.auth.auth_handler import signAndGetJWT
 from app.my_sql_connection_cursor import cursor, connection # type: ignore
 
 users_router = APIRouter()
@@ -9,17 +10,25 @@ async def update_profile_picture(request: Request):
     request_json = await request.json()
     username = request_json.get('username')
     image_url = request_json.get('image_url')
-    query = f"UPDATE `user` SET `image_url`='{image_url}' WHERE `username`='{username}'"
+    role = request_json.get('role')
+    name = request_json.get('name')
+
+    print(username, image_url, role, name)
+
+    query = f"UPDATE `user` SET `image_path`='{image_url}' WHERE `username`='{username}'"
     try:
         cursor.execute(query)
         connection.commit()
-        return {
-            "status": True,
-            "msg": "Profile picture updated successfully"
-        }
     except Exception as e:
         print(e)
         return {
             "status": False,
             "msg": "Something went wrong"
         }
+    
+    return {
+        "token": signAndGetJWT({"username": username, "role": role, "name": name, "image_url": image_url}), 
+        "status": True,
+        "msg": "Profile picture updated successfully"
+    }
+    
